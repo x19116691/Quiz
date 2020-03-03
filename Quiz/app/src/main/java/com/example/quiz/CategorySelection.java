46f34e7f1cee444029d5db6e5b39590371a67cb0
Conor-Corboy <58186008+x19116691@users.noreply.github.com>
2020-03-03 10:56:00 +0000

    Add files via upload

---
+++ Quiz/app/src/main/java/com/example/quiz/CategorySelection.java
@@ -0,0 +1,17 @@
+package com.example.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CategorySelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_selection);
+    }
+}
